d3de15da807a88736d4d8cb0bfad412169713845
Tri Duong <duongthuantri159@gmail.com>
2024-06-22 17:26:17 +0700

    Refactor: Automate the process of move dist files to root direc

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
 
   return (
-    <div className="">This is XploreSaigon</div>
+    <div className="">This is XploreSaigon. Exploring Saigon by your way!</div>
   )
 }
 
