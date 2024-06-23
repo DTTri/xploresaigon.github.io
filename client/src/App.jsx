a74619aa90e64ba806b39e65af36d769f0a0f1ca
Tri Duong <duongthuantri159@gmail.com>
2024-06-23 10:55:18 +0700

    Feat: Internationalization - Vietnamese and English

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
 import './App.css'
-
+import { useTranslation } from 'react-i18next';
+import './i18n';
 function App() {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  }
 
   return (
-    <div className="">This is XploreSaigon. Exploring Saigon by your way!</div>
+    <header>
+      <h1>{t('title')}</h1>
+      <nav>
+        <ul>
+          <li><a href="#">{t('nav.explore')}</a></li>
+        </ul>
+      </nav>
+      <button onClick={() => changeLanguage('en')}>English</button>
+      <button onClick={() => changeLanguage('vi')}>Tiếng Việt</button>
+      <h1>Hello</h1>
+    </header>
   )
 }
 
