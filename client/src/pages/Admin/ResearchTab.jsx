f74a7dc2f96ad99ffc4bb6f15a533d2c7c5c9f37
Tri Duong <duongthuantri159@gmail.com>
2024-07-23 17:27:41 +0700

    Feat: ResearchTab- add react-chartjs-2 dependency

--- client/src/pages/Admin/ResearchTab.jsx
+++ client/src/pages/Admin/ResearchTab.jsx
@@ -1,7 +1,107 @@
+import { Line } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+} from 'chart.js';
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler
+);
+
 export default function ResearchTab() {
+  const accessData = {
+    labels: [
+      'Tháng 1',
+      'Tháng 2',
+      'Tháng 3',
+      'Tháng 4',
+      'Tháng 5',
+      'Tháng 6',
+      'Tháng 7',
+    ],
+    datasets: [
+      {
+        label: 'Lưu lượng truy cập',
+        // hardcode data for now
+        data: [0, 150, 160, 430, 700, 1200, 1500],
+        fill: true,
+        backgroundColor: 'rgba(116,39,116, 0.2)',
+        borderColor: 'rgba(116,39,116, 1)',
+      },
+    ],
+  };
+  const interactionsData = {
+    labels: [
+      'Tháng 1',
+      'Tháng 2',
+      'Tháng 3',
+      'Tháng 4',
+      'Tháng 5',
+      'Tháng 6',
+      'Tháng 7',
+    ],
+    datasets: [
+      {
+        label: 'Mức độ tương tác',
+        // hardcode data for now
+        data: [0, 320, 260, 480, 900, 1050, 1500],
+        fill: true,
+        backgroundColor: 'rgba(116,170,238, 0.2)',
+        borderColor: 'rgba(116,170,238, 1)',
+      },
+    ],
+  };
+  const options1 = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Biểu đồ lưu lượng truy cập',
+        color: 'black',
+        font: {
+          size: 24,
+        },
+      },
+    },
+  };
+  const options2 = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Biểu đồ mức độ tương tác',
+        color: 'black',
+        font: {
+          size: 24,
+        },
+      },
+    },
+  };
   return (
-    <div>
-      <h1>Khảo sát</h1>
+    <div className='research-tab flex flex-col items-center gap-8'>
+      <div className='charts-container w-3/4 p-4 '>
+        <Line data={accessData} options={options1} className='mb-16' />
+        <Line data={interactionsData} options={options2} />
+      </div>
     </div>
   );
 }
--- client/src/pages/Admin/TagsTab.jsx
+++ client/src/pages/Admin/TagsTab.jsx
@@ -1,7 +1,106 @@
 export default function TagsTab() {
+  const tagsList = [
+    {
+      id: 1,
+      title: '5 sao',
+      category: 'Ăn uống',
+    },
+    {
+      id: 1,
+      title: '5 sao',
+      category: 'Ăn uống',
+    },
+    {
+      id: 1,
+      title: '5 sao',
+      category: 'Ăn uống',
+    },
+    {
+      id: 1,
+      title: '5 sao',
+      category: 'Ăn uống',
+    },
+    {
+      id: 1,
+      title: '5 sao',
+      category: 'Ăn uống',
+    },
+    {
+      id: 1,
+      title: '5 sao',
+      category: 'Ăn uống',
+    },
+    {
+      id: 1,
+      title: '5 sao',
+      category: 'Ăn uống',
+    },
+    {
+      id: 1,
+      title: '5 sao',
+      category: 'Ăn uống',
+    },
+    {
+      id: 1,
+      title: '5 sao',
+      category: 'Ăn uống',
+    },
+  ];
+
   return (
-    <div>
-      <h1>Quản lí tags</h1>
+    <div className='tags-tab'>
+      <div className='filters--container flex gap-8 flex-wrap justify-start items-center'>
+        <div className='filters--items filter--search-by-name'>
+          <select
+            name='searchByName'
+            id='searchByName'
+            className='filter--search-by-name__select'
+          >
+            <option value='title'>Tên thẻ</option>
+            <option value='id'>ID</option>
+          </select>
+          <input
+            type='text'
+            className='filter--search-by-name__input min-w-80'
+            placeholder='Tên thẻ/ID'
+          />
+        </div>
+
+        <div className='filters--items filter--category'>
+          <label htmlFor='category'>Lĩnh vực</label>
+          <select name='category' id='category'>
+            <option value='all'>Tất cả</option>
+            <option value='food'>Ẩm thực</option>
+            <option value='stay'>Lưu trú</option>
+            <option value='visit'>Địa điểm tham quan</option>
+            <option value='festival'>Lễ hội</option>
+          </select>
+        </div>
+
+        <button className='new-tag__button bg-primary font-bold p-2 rounded-md'>
+          Thêm thẻ mới
+        </button>
+      </div>
+      <div className='tags-list-container mt-8  max-h-[400px] overflow-auto'>
+        <table className='tags-list w-full'>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Tên thẻ</th>
+              <th>Lĩnh vực</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tagsList.map((tag) => (
+              <tr key={tag.id}>
+                <td>{tag.id}</td>
+                <td>{tag.title}</td>
+                <td>{tag.category}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
